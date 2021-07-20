2a6f8d2783d6e014fae8a6fc5efaf18bc967d4b7
1712859 <tubato1999@gmail.com>
2021-07-20 23:00:18 +0700

    thêm đặt câu hỏi ở trang chủ, trang câu hỏi chi tiết

--- app/controllers/questionsController.php
+++ app/controllers/questionsController.php
@@ -8,33 +8,50 @@ class QuestionsController
         require_once "./app/core/callapi.php";
         require_once "./app/config.php";
         $callapi = new callapi();
-        $us = [
-            "id_check" => $id
-        ];
 
-        $response = $callapi->callAPI('POST', _API_ROOT.'question/readone.php', json_encode($us));
-        $question =  $response["data"];
+        $temp_id = [
+            "id"=>$id
+        ];
+        $response0 = $callapi->callAPI('POST', _API_ROOT.'question/getmaxid.php', json_encode($temp_id));
         
-        $us1 = [
-            "ques_id" => $id
-        ];
-
-        $response1 = $callapi->callAPI('POST', _API_ROOT.'answer/getByidQues.php', json_encode($us1));
-        $answer =  $response1["data"];
-
-        if(isset($response1["data"][0]))
+        if($response0["data"]["max"] == true)
         {
-            $question["anser"] = count($answer);
+            $us = [
+                "id_check" => $id
+            ];
+    
+            $response = $callapi->callAPI('POST', _API_ROOT.'question/readone.php', json_encode($us));
+            $question =  $response["data"];
+            
+            $us1 = [
+                "ques_id" => $id
+            ];
+    
+            $response1 = $callapi->callAPI('POST', _API_ROOT.'answer/getByidQues.php', json_encode($us1));
+            $answer =  $response1["data"];
+    
+            if(isset($response1["data"][0]))
+            {
+                $question["anser"] = count($answer);
+            }
+            else
+            {
+                $question["anser"] = 0;
+                $answer = null;
+            }
+            
+            $VIEW = "./app/views/question/question_detail.phtml";
+            require("./app/layouts/questionLayout.phtml");
         }
         else
         {
-            $question["anser"] = 0;
-            $answer = null;
+            
+            require("./app/views/error/404.php");
         }
         
+        
 
-        $VIEW = "./app/views/question/question_detail.phtml";
-        require("./app/layouts/questionLayout.phtml");
+        
     }
     
     
--- app/controllers/userProfileController.php
+++ app/controllers/userProfileController.php
@@ -4,64 +4,81 @@ class userProfileController
 {
     public function index($id)
     {   
+        
         require_once "./app/core/callapi.php";
         require_once "./app/config.php";
         $callapi = new callapi();
-        try
-        {
-            $response = $callapi->callAPI('GET', _API_ROOT.'user_account/read_one.php?id_user='.$id, null);
-            if($response["code"]>=400)
-                {
-                    echo $response["data"]["message"];
-                
-                }else
-                {
-                    $response1 = $callapi->callAPI('GET', _API_ROOT.'user_account/getNumQues.php?id_user='.$id, null);
-                    if($response1["code"]>=400)
-                        {
-                            echo $response1["data"]["message"];
-                        
-                        }else
-                        {
-                            $user = [
-                                "id" => $response["data"]["id_user"],
-                                "email" => $response["data"]["email"],
-                                "name" => $response["data"]["name"],
-                                "image" => $response["data"]["image"],
-                                "phone" => $response["data"]["phone"],
-                                "birth" => $response["data"]["birth"],
-                                "created" => $response["data"]["created"],
-                                "ques" => $response1["data"]["ques"],
-                                "answer" => $response1["data"]["answer"],
-                            ];
 
-                            $response2 = $callapi->callAPI('GET', _API_ROOT.'category/read-all.php', null);
-                            $category = $response2["data"]["res"]["categories"];
+        $temp_id = [
+            "id"=>$id
+        ];
+        $response0 = $callapi->callAPI('POST', _API_ROOT.'user_account/getmaxid.php', json_encode($temp_id));
+        
+        if($response0["data"]["max"] == true)
+        {
+            try
+            {
+                $response = $callapi->callAPI('GET', _API_ROOT.'user_account/read_one.php?id_user='.$id, null);
+                if($response["code"]>=400)
+                    {
+                        echo $response["data"]["message"];
+                    
+                    }else
+                    {
+                        $response1 = $callapi->callAPI('GET', _API_ROOT.'user_account/getNumQues.php?id_user='.$id, null);
+                        if($response1["code"]>=400)
+                            {
+                                echo $response1["data"]["message"];
                             
-                            $dataa = [
-                                "owner_id" => $user["id"]
-                            ];
-                            $response3 = $callapi->callAPI('POST', _API_ROOT.'question/get_by_owner_id.php', json_encode($dataa));
-                            if(isset($response3["data"]["res"]))
+                            }else
                             {
-                                $question = $response3["data"]["res"];
-                            }
-                            else
-                            {
-                                $question = null; 
-                            }
+                                $user = [
+                                    "id" => $response["data"]["id_user"],
+                                    "email" => $response["data"]["email"],
+                                    "name" => $response["data"]["name"],
+                                    "image" => $response["data"]["image"],
+                                    "phone" => $response["data"]["phone"],
+                                    "birth" => $response["data"]["birth"],
+                                    "created" => $response["data"]["created"],
+                                    "ques" => $response1["data"]["ques"],
+                                    "answer" => $response1["data"]["answer"],
+                                ];
 
-                            $VIEW = "./app/views/user/userProfile.phtml";
-                            require("./app/layouts/questionLayout.phtml");
-                        }
-                }
-            
+                                $response2 = $callapi->callAPI('GET', _API_ROOT.'category/read-all.php', null);
+                                $category = $response2["data"]["res"]["categories"];
+                                
+                                $dataa = [
+                                    "owner_id" => $user["id"]
+                                ];
+                                $response3 = $callapi->callAPI('POST', _API_ROOT.'question/get_by_owner_id.php', json_encode($dataa));
+                                if(isset($response3["data"]["res"]))
+                                {
+                                    $question = $response3["data"]["res"];
+                                }
+                                else
+                                {
+                                    $question = null; 
+                                }
+
+                                $VIEW = "./app/views/user/userProfile.phtml";
+                                require("./app/layouts/questionLayout.phtml");
+                            }
+                    }
+                
+            }
+            catch(Exception $e)
+            {
+                $VIEW = "./app/views/user/userProfile.phtml";
+                require("./app/layouts/questionLayout.phtml");
+            };
         }
-        catch(Exception $e)
+        else
         {
-            $VIEW = "./app/views/user/userProfile.phtml";
-            require("./app/layouts/questionLayout.phtml");
-        };
+            
+            require("./app/views/error/404.php");
+        }
+
+        
         
     }
     

b2d6a70f8a7c5739e267a8d0676420a6fe10305f
Duy Quang <61661668+duyquang19981@users.noreply.github.com>
2021-08-10 17:45:29 +0700

    re-url, web root, api root

--- app-admin/configs.php
+++ app-admin/configs.php
@@ -7,20 +7,21 @@ if (!empty($_SERVER['HTTPS']) && $_SERVER('HTTP') == "on") {
 } else {
   $web_root = 'http://' . $_SERVER['HTTP_HOST'];
 }
-$temp = str_replace("\\", "/", strtolower(_DIR_ROOT));
-$folder = str_replace(strtolower($_SERVER['DOCUMENT_ROOT']), '', $temp);
+// $temp = str_replace("\\", "/", strtolower(_DIR_ROOT));
+// $folder = str_replace(strtolower($_SERVER['DOCUMENT_ROOT']), '', $temp);
+$folder = '/app-admin';
 $web_root = $web_root . $folder;
 
 define('_WEB_ROOT', $web_root);
-
 $public = _WEB_ROOT . '/public';
 define('_PUBLIC', $public);
 
-$containerFolder = $web_root . "/..";
-define('_CONTAINER_FOLDER', $containerFolder);
+// $containerFolder = $web_root . "/..";
+// define('_CONTAINER_FOLDER', $containerFolder);
 
-$current_admin_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-$indexOfReplaceString = strpos($current_admin_url, "app-admin");
+// $current_admin_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+// $indexOfReplaceString = strpos($current_admin_url, "app-admin");
 
-$api_url = substr_replace($current_admin_url, "api/data_api", $indexOfReplaceString);
+// $api_url = substr_replace($current_admin_url, "api/data_api", $indexOfReplaceString);
+$api_url = _WEB_ROOT . '/api/data_api';
 define('_API_ROOT', $api_url);
